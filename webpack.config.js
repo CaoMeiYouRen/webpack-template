0c844bee9a812967811ffe29e93bc74ccdd221b4
CaoMeiYouRen <996881204@qq.com>
2020-11-26 20:05:25 +0800

    feat: 优化 生成类型文件；开发编译

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 const _ = require('lodash')
 const { name } = require('./package.json')
